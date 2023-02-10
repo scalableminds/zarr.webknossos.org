4580db69b3075aabbe13d6bfa894294ed34c999a
William Moore <w.moore@dundee.ac.uk>
2023-02-10 16:29:07 +0000

    Add vite and netlify configs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+let config = {
   plugins: [react()]
-})
+};
+// this will be undefined when deployed from netlify, but is used by gh-pages
+if (process.env.GITHUB_REPOSITORY_OWNER) {
+  config.base = `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/ome-ngff-validator/`;
+}
+
+export default defineConfig(config);
